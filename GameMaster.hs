56a80d62853d68a3951611c624fc202871428fe9
Eric McGarry <46828798+mcgarrye@users.noreply.github.com>
2019-03-22 14:14:20 -0400

    Addition of all files
    Part 1 completed

---
+++ GameMaster.hs
@@ -0,0 +1,56 @@
+module GameMaster where
+
+import Control.Monad (ap, liftM)
+
+import GameMasterDef
+
+-- Question 1.
+
+-- The game master for the guessing game.  The parameter is the secret number
+-- the player has to guess.
+guessingGame :: MonadGameMaster m => Integer -> m Ending
+guessingGame secret
+    | secret < 1 || secret > 100 = error "invalid game"
+    | otherwise = nextTurn 1 100 
+    where nextTurn low high = do
+        req1 <- gmAction low high
+        case req1 of
+            Surrender -> return (Lose secret)
+            Guess i
+                | i == secret -> return Win
+                | i > high || i < low -> return (nextTurn low high)
+                | i > secret -> return (nextTurn low (high-1))
+                | otherwise -> return (nextTurn (low+1) high)
+
+
+-- Question 2.
+
+instance Functor FreeGameMaster where
+    -- fmap :: (a -> b) -> FreeGameMaster a -> FreeGameMaster b
+    -- If you are confident with your Monad instance, you can just write
+    -- fmap = liftM
+    fmap f (FreeGameMaster x) = FreeGameMaster (f x)
+
+instance Applicative FreeGameMaster where
+    -- pure :: a -> FreeGameMaster a
+    -- If you are confident with your Monad instance, you can just write
+    -- pure = return
+    pure x = FreeGameMaster x
+
+    -- (<*>) :: FreeGameMaster (a -> b) -> FreeGameMaster a -> FreeGameMaster b
+    -- If you are confident with your Monad instance, you can just write
+    -- (<*>) = ap
+
+instance Monad FreeGameMaster where
+    -- return :: a -> FreeGameMaster a
+
+    -- (>>=) :: FreeGameMaster a -> (a -> FreeGameMaster b) -> (FreeGameMaster b)
+
+instance MonadGameMaster FreeGameMaster where
+    -- gmAction :: Integer -> Integer -> FreeGameMaster PlayerMsg
+
+
+-- Question 3.
+
+testGame :: (Integer -> FreeGameMaster Ending) -> Bool
+testGame = error "TODO"
