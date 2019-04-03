1d4f41873aa461dd22b48d76841cd2d8279a8917
Eric <eric.mcgarry@mail.utoronto.ca>
2019-04-02 22:10:59 -0400

    Real A4

---
+++ A4.hs
@@ -0,0 +1,23 @@
+module A4 where
+
+import           Control.Applicative
+import           Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+
+import           A4Def
+import           ParserLib
+
+-- This can help testing by reading from a file so you can test multi-line input
+-- and also have little hassle with \
+parseFile :: String -> IO (Maybe Expr)
+parseFile filename = do
+    inp <- readFile filename
+    let ans = runParser mainParser inp
+    return ans
+
+mainParser :: Parser Expr
+mainParser = error "TODO"
+
+
+mainInterp :: Expr -> Either Error Value
+mainInterp = error "TODO"
