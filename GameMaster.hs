ef45d4d8af1030ba523f2c5d63a1df06b9525754
Eric <eric.mcgarry@mail.utoronto.ca>
2019-04-02 22:10:36 -0400

    A4

--- GameMaster.hs
+++ GameMaster.hs
@@ -1,6 +1,7 @@
 module GameMaster where
 
 import Control.Monad (ap, liftM)
+import Data.Maybe
 
 import GameMasterDef
 
@@ -9,7 +10,7 @@ import GameMasterDef
 -- The game master for the guessing game.  The parameter is the secret number
 -- the player has to guess.
 guessingGame :: MonadGameMaster m => Integer -> m Ending
-guessingGame secretl
+guessingGame secret
     | secret < 1 || secret > 100 = error "invalid game"
     | otherwise = nextTurn 1 100
     where 
@@ -42,19 +43,54 @@ instance Applicative FreeGameMaster where
 
 instance Monad FreeGameMaster where
     -- return :: a -> FreeGameMaster a
-    return a -> Pure a
+    return a = Pure a
 
     -- (>>=) :: FreeGameMaster a -> (a -> FreeGameMaster b) -> (FreeGameMaster b)
     Pure a >>= k = k a
-    (GMAction low high next) >>= k = 
-        case next response
-
+    GMAction low high next >>= k =  GMAction low high (\message -> (next message >>= k))
 
 instance MonadGameMaster FreeGameMaster where
     -- gmAction :: Integer -> Integer -> FreeGameMaster PlayerMsg
+    gmAction low high = GMAction low high (\message -> Pure message)
 
 
 -- Question 3.
 
 testGame :: (Integer -> FreeGameMaster Ending) -> Bool
-testGame = error "TODO"
+testGame game = isJust(testcase game)
+
+testcase :: (Integer -> FreeGameMaster Ending) -> Maybe()
+testcase game = do 
+    f1 <- thirdHelper 1 100 (game 20)
+    firstHelper (f1 (Guess 20))
+    secondHelper (f1 Surrender) 20
+    f2 <- thirdHelper 11 100 (f1 (Guess 10))
+    firstHelper (f2 (Guess 20))
+    secondHelper (f2 Surrender) 20
+    f3 <- thirdHelper 11 67 (f2 (Guess 68))
+    firstHelper (f3 (Guess 20))
+    secondHelper (f3 Surrender) 20
+    f4 <- thirdHelper 11 67 (f3 (Guess 70))
+    firstHelper (f4 (Guess 20))
+    secondHelper (f4 Surrender) 20
+    f5 <- thirdHelper 11 67 (f3 (Guess 200))
+    firstHelper (f5 (Guess 20))
+    secondHelper (f5 Surrender) 20
+
+
+
+firstHelper :: FreeGameMaster Ending -> Maybe()
+firstHelper (Pure Win) = Just()
+firstHelper _ = Nothing
+
+secondHelper :: FreeGameMaster Ending -> Integer -> Maybe()
+secondHelper (Pure (Lose n)) secret
+    | n == secret = Just()
+    | otherwise = Nothing
+secondHelper _ secret = Nothing
+
+thirdHelper :: Integer -> Integer -> FreeGameMaster Ending -> Maybe(PlayerMsg -> FreeGameMaster Ending)
+thirdHelper low high (GMAction i j k)
+    | (low == i) && (high == j) = Just k
+    | otherwise = Nothing
+thirdHelper _ _ (Pure _) = Nothing
