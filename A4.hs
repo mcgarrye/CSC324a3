aa8afc429665998d45baaf467eb0b1f553e44d8f
Eric <eric.mcgarry@mail.utoronto.ca>
2019-04-04 21:44:48 -0400

    interpretter is done

--- A4.hs
+++ A4.hs
@@ -3,7 +3,7 @@ module A4 where
 import           Control.Applicative
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-
+import           Control.Monad
 import           A4Def
 import           ParserLib
 
@@ -62,4 +62,97 @@ mainParser = whitespaces *> block <* eof
 
 
 mainInterp :: Expr -> Either Error Value
-mainInterp = error "TODO"
+mainInterp = interp Map.empty
+
+intOrDie :: Value -> Either Error Integer
+intOrDie (VN i) = pure i
+intOrDie _ = Left TypeError
+
+interp :: Map String Value -> Expr -> Either Error Value
+interp _ (Num i) = pure (VN i)
+
+interp _ (Bln b) = pure (VB b)
+
+interp env (Var v) = case Map.lookup v env of
+  Just a -> pure a
+  Nothing -> Left VarNotFound
+
+interp env (Prim2 Plus e1 e2) = do
+    a <- interp env e1
+    i <- intOrDie a
+    b <- interp env e2
+    j <- intOrDie b
+    pure (VN (i + j))
+
+interp env (Prim2 Minus e1 e2) = do
+    a <- interp env e1
+    i <- intOrDie a
+    b <- interp env e2
+    j <- intOrDie b
+    pure (VN (i - j))
+
+interp env (Prim2 Mul e1 e2) = do
+    a <- interp env e1
+    i <- intOrDie a
+    b <- interp env e2
+    j <- intOrDie b
+    pure (VN (i * j))
+
+interp env (Prim2 Div e1 e2) = do
+    a <- interp env e1
+    i <- intOrDie a
+    b <- interp env e2
+    j <- intOrDie b
+    case j of
+        0 -> Left DivByZero
+        _ -> pure (VN (div i j))
+
+interp env (Prim2 Mod e1 e2) = do
+    a <- interp env e1
+    i <- intOrDie a
+    b <- interp env e2
+    j <- intOrDie b
+    case j of
+        0 -> Left DivByZero
+        _ -> pure (VN (mod i j))
+
+interp env (Prim2 Eq e1 e2) = do
+    a <- interp env e1
+    i <- intOrDie a
+    b <- interp env e2
+    j <- intOrDie b
+    pure (VB (i == j))
+
+interp env (Prim2 Lt e1 e2) = do
+    a <- interp env e1
+    i <- intOrDie a
+    b <- interp env e2
+    j <- intOrDie b
+    pure (VB (i < j))
+
+interp env (Cond test eThen eElse) = do
+    a <- interp env test
+    case a of
+      VB True -> interp env eThen
+      VB False -> interp env eElse
+      _ -> Left TypeError
+
+interp env (Let eqns evalMe) = do
+    env' <- extend env eqns
+    interp env' evalMe
+  where
+    extend = foldM extendOnce
+    extendOnce env (v,rhs) = do
+        a <- interp env rhs
+        pure (Map.insert v a env)
+
+interp env (Lambda v body) = pure (VClosure env v body)
+
+interp env (App f e) = do
+    c <- interp env f
+    case c of
+      VClosure fEnv v body -> do
+          eVal <- interp env e
+          let bEnv = Map.insert v eVal fEnv  -- fEnv, not env
+          interp bEnv body
+      _ -> Left TypeError
